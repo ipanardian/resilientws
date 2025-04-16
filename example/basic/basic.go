4a27e293654f85e61e066c189eb4ed84b83391c5
Ipan Ardian <ardian.ipan@gmail.com>
2025-04-17 00:53:22 +0700

    feat: add basic example

---
+++ example/basic/basic.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/url"
+	"time"
+
+	"github.com/ipanardian/resilientws"
+)
+
+var ws *resilientws.Resws
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		time.Sleep(10 * time.Second)
+		log.Println("Closing connection after 10 seconds")
+		cancel()
+	}()
+
+	Run()
+
+	<-ctx.Done()
+	ws.Close()
+}
+
+func Run() {
+	ws = &resilientws.Resws{
+		MessageHandler:   MessageHandler,
+		PingHandler:      PingHandler,
+		PingInterval:     5 * time.Second,
+		MessageQueueSize: 1,
+	}
+
+	// sends a message after connection is established with onConnected event
+	ws.OnConnected(func(url string) {
+		err := ws.WriteMessage(1, []byte("Hello"))
+		if err != nil {
+			log.Println("Write message error:", err)
+		}
+		log.Println("Hello sent")
+	})
+
+	// sends a message while connection is not established
+	// this message will be queued and sent after connection is established
+	ws.Send([]byte("This is a queued message"))
+
+	u := url.URL{Scheme: "wss", Host: "echo.websocket.org"}
+	ws.Dial(u.String())
+}
+
+func PingHandler() {
+	log.Println("Send ping")
+	err := ws.Send([]byte(`ping`))
+	if err != nil {
+		log.Println("Ping error:", err)
+	}
+}
+
+func MessageHandler(_ int, msg []byte) {
+	log.Println("Received message:", string(msg))
+}
